5525d28a9e46578bdacb6ad38b0e189fe9528d5e
Heather Walker <cookiesncream@ladle.net>
2016-05-02 21:47:59 -0700

    added hungry belly screen shots

--- public/js/controllers/project2Controller.js
+++ public/js/controllers/project2Controller.js
@@ -1,4 +1,5 @@
 portfolio.controller('project2Controller', function($document, $scope){
 	$scope.interval = 500;
 	$scope.active = 0;
+	$scope.images = ['hb_login.png', 'hb_friend.png', 'hb_edit.png', 'hb_invite.png', 'hb_map.png']
 })
