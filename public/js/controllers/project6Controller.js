dda1c6ca0732d212ea78280854cb7a62aa44a439
Heather Walker <cookiesncream@ladle.net>
2016-05-09 10:08:02 -0700

    info in about

--- public/js/controllers/project6Controller.js
+++ public/js/controllers/project6Controller.js
@@ -1,4 +1,5 @@
 portfolio.controller('project6Controller', function($document, $scope){
 	$scope.interval = 500;
 	$scope.active = 0;
+	$scope.images = ['recipe_index.png', 'recipe_create.png', 'recipe_box.png', 'recipe_notes.png', 'recipe_recipe.png', 'recipe_add.png']
 })
