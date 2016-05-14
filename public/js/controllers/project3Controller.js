ff66411d8b40fb488bc3053de17cf73350ca26a9
Heather Walker <cookiesncream@ladle.net>
2016-05-13 17:42:31 -0700

    fixed pictures

--- public/js/controllers/project3Controller.js
+++ public/js/controllers/project3Controller.js
@@ -1,5 +1,5 @@
 portfolio.controller('project3Controller', function($document, $scope){
 	$scope.myInterval = 3000;
 	$scope.active = 0;
-	$scope.slides = [{id:0, image: '/static/happen_index.png'}, {id:1, image: '/static/happen_add.png'}, {id:2, image: '/static/happen_profile.png'}, {id:3, image: '/static/happen_show_pic.png'}, {id:4, image: '/static/happen_show_tweet.png'}, {id:5, image: '/static/happen_tweepy.png'}]
+	$scope.slides = [{id:0, image: '/static/happen_index.png'}, {id:1, image: '/static/happen_add.png'}, {id:2, image: '/static/happen_profile.png'}, {id:3, image: '/static/happen_pics.png'}, {id:4, image: '/static/happen_tweets.png'}, {id:5, image: '/static/happen_show_pic.png'}, {id:6, image: '/static/happen_tweet_profile.png'}]
 })
