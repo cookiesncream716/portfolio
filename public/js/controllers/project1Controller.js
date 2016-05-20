716b12730d2fcd7bb7ea7130a79b0b90bba1aeb2
Heather Walker <cookiesncream@ladle.net>
2016-05-19 22:41:33 -0700

    social media icons

--- public/js/controllers/project1Controller.js
+++ public/js/controllers/project1Controller.js
@@ -1,4 +1,5 @@
 portfolio.controller('project1Controller', function($document, $scope){
-	$scope.interval = 500;
+	$scope.myInterval = 3000;
 	$scope.active = 0;
+	$scope.slides = [{id:0, image: '/static/shoe_index.png'}, {id: 1, image: '/static/shoe_dashboard1.png'}, {id: 2, image: '/static/shoe_dashboard2.png'}, {id: 3, image: '/static/shoe_all.png'}]
 })
