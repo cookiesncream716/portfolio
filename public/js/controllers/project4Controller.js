5ee7b9a0486239f53f5fa743dc6763d4a7cec957
Heather Walker <cookiesncream@ladle.net>
2017-03-06 10:17:10 -0800

    added pics to discussion board

--- public/js/controllers/project4Controller.js
+++ public/js/controllers/project4Controller.js
@@ -1,5 +1,5 @@
 portfolio.controller('project4Controller', function($document, $scope){
 	$scope.myInterval = 3000;
 	$scope.active = 0;
-	$scope.slides = [{id:0, image: '/static/shoe_index.png'}, {id: 1, image: '/static/shoe_dashboard1.png'}, {id: 2, image: '/static/shoe_dashboard2.png'}, {id: 3, image: '/static/shoe_all.png'}]
+	$scope.slides = [{id:0, image: '/static/discuss_login.png'}, {id: 1, image: '/static/discuss_dashboard.png'}, {id: 2, image: '/static/discuss_topics.png'}, {id: 3, image: '/static/discuss_profile.png'}]
 })
